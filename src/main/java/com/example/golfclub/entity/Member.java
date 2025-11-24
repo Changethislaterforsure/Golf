da89d479b61aa2144ee6c116fbd41c08917be45a
changethislaterforsure <benjmain.legge@keyin.comm>
2025-11-24 15:48:38 -0330

    created entity classes, pom.xml and skeleton of main class

--- src/main/java/com/example/golfclub/entity/Member.java
+++ src/main/java/com/example/golfclub/entity/Member.java
@@ -1,1 +1,105 @@
+package com.example.golfclub.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String memberName;
+    private String memberAddress;
+    private String memberEmail;
+    private String memberPhone;
+
+    private LocalDate membershipStartDate;
+    private Integer membershipDurationMonths; // duration in months
+
+    // Not in the bullets, but needed for "search by membership type"
+    private String membershipType;
+
+    @ManyToMany(mappedBy = "participants")
+    private Set<Tournament> tournaments = new HashSet<>();
+
+    public Member() {
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberAddress() {
+        return memberAddress;
+    }
+
+    public void setMemberAddress(String memberAddress) {
+        this.memberAddress = memberAddress;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public String getMemberPhone() {
+        return memberPhone;
+    }
+
+    public void setMemberPhone(String memberPhone) {
+        this.memberPhone = memberPhone;
+    }
+
+    public LocalDate getMembershipStartDate() {
+        return membershipStartDate;
+    }
+
+    public void setMembershipStartDate(LocalDate membershipStartDate) {
+        this.membershipStartDate = membershipStartDate;
+    }
+
+    public Integer getMembershipDurationMonths() {
+        return membershipDurationMonths;
+    }
+
+    public void setMembershipDurationMonths(Integer membershipDurationMonths) {
+        this.membershipDurationMonths = membershipDurationMonths;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    public Set<Tournament> getTournaments() {
+        return tournaments;
+    }
+
+    public void setTournaments(Set<Tournament> tournaments) {
+        this.tournaments = tournaments;
+    }
+}
